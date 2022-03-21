cddf9d201edc62cd22fe59acb114f314396b7bc0
stefanhugl <102042196+stefanhugl@users.noreply.github.com>
2022-03-21 15:44:24 +0100

    WasserstoffOrbitale.java erstellt

---
+++ WasserstoffOrbitale.java
@@ -0,0 +1,15 @@
+package de.kratzer.horb;
+
+// kurze Variablen
+// n, l, m		Quantenzahlen
+// x, y, z, t	Koordinaten
+// h, b  		Bildschirmhöhe, -breite
+// v			Vergrößerung
+
+public class WasserstoffOrbitale {
+	
+	public static void main(String[] args) {
+		
+		Rahmen OrbitalRahmen = new Rahmen();
+	}
+}
